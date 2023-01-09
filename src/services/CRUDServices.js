7f7cb0b62e6ff5d7b4db7e2ad3e3d37292bb8811
phongnt <bongdem30101998@gmail.com>
2023-01-09 22:56:31 +0700

    feat(editCRUD): Sử Dụng ORM Để Sửa Dữ liệu

--- src/services/CRUDServices.js
+++ src/services/CRUDServices.js
@@ -50,7 +50,53 @@ let getAllUser = () => {
   });
 };
 
+let getUserInfoById = (userId) => {
+  return new Promise(async(resolve, reject) => {
+    try{
+      let user = await db.User.findOne({
+        where: {id: userId},
+        raw: true
+      })
+      if(user) {
+        resolve(user)
+      } else {
+        resolve([])
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: {id: data.id}
+      })
+      if(user) {
+        user.firstName = data.firstName,
+        user.lastName = data.lastName,
+        user.address = data.address,
+
+        await user.save()
+        // sau khi update user xong gọi all user bằng cách
+        let allUser = db.User.findAll({
+          raw: true,
+        })
+        resolve(allUser)
+      } else {
+        resolve()
+      }
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
 module.exports = {
   createNewUser: createNewUser,
   getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData,
 };
